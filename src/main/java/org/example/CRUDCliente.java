3e94c9d21ad886852d777c7ea3d8236791da1da0
Eduu2574 <eduardo.dmztrb@gmail.com>
2024-09-19 11:44:37 +0200

    commit

---
+++ src/main/java/org/example/CRUDCliente.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class CRUDCliente {
+
+    public static ArrayList<Cliente> clientes;
+    public static void buscarUsuario(ArrayList lista, String correo) {
+
+    }
+
+/*    public static void totalIngre(ArrayList lista, String correo) {}
+
+     */
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,34 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int opcion = sc.nextInt();
+            System.out.println("1. Añadir usuario\n2.Buscar usuario\n3.Total ingresos\n0. Salir");
+            switch (opcion) {
+                case 0:
+                    break;
+                case 1:
+                    String correo = sc.next();
+                    Usuario usuario = new Usuario();
+                    usuario.setIdentificador(correo);
+                    CRUDCliente.clientes.add(usuario);
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.printf("No existe dicha opción");
+                    break;
+            }
 
-      Usuario usuario = new Usuario();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        Usuario usuario = new Usuario();
     }
 }
